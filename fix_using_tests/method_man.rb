719874326e0468e5c7ac243a2ffedff195570525
Steven Kandow <kando1sr@gmail.com>
2020-03-25 16:48:39 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -1,11 +1,11 @@
 require 'pry'
 
 def start_game(player1, player2)
-  greeting = "Hello #{player1} & #{playar2}"
+  greeting = "Hello #{player1} & #{player2}"
   greeting 
 end
 
-def play_game(player2)
+def play_game(player1, player2)
   greeting = "#{player1} is better than #{player2}"
   greeting 
 end
